ce28e74239a4ccb50cff4e9d9491df88c54af1b2
Jozef Vaclavik <jozef@hey.com>
2021-12-16 10:23:19 +0000

    fix: correct module/class definition

--- lib/ledger_sync/domains/operation/resource.rb
+++ lib/ledger_sync/domains/operation/resource.rb
@@ -1,24 +1,25 @@
 # frozen_string_literal: true
+
 module LedgerSync
-    module Domains
-      module Operation
-        class Resource
-          include LedgerSync::Domains::Operation::Mixin
-  
-          def resource_class
-            @resource_class ||= inferred_resource_class
-          end
-  
-          private
-  
-          def inferred_resource_class
-            name = self.class.to_s.split('::')
-            name.pop # remove serializer/operation class from name
-            resource = name.pop.singularize # pluralized resource module name
-  
-            const_get((name + [resource]).join('::'))
-          end
+  module Domains
+    class Operation
+      class Resource
+        include LedgerSync::Domains::Operation::Mixin
+
+        def resource_class
+          @resource_class ||= inferred_resource_class
+        end
+
+        private
+
+        def inferred_resource_class
+          name = self.class.to_s.split('::')
+          name.pop # remove serializer/operation class from name
+          resource = name.pop.singularize # pluralized resource module name
+
+          self.class.const_get((name + [resource]).join('::'))
         end
       end
     end
   end
+end
